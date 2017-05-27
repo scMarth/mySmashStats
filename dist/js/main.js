3309133e14035a1bdca3d9e67af6a79edf5ab22e
scMarth <vlantaca@gmail.com>
2017-05-26 17:40:15 -0700

    button click visual effects

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 var matchesDiv = $('#matches-div')[0];
+var statsDiv = $('#stats-div')[0];
 var statsObject = {};
 
 // Show / Hide Matches Button
@@ -71,45 +72,44 @@ function getStageImgString(stageStr){
    return output;
 }
 
-function getCharImgString(dataArray){
-   var charStr = dataArray[0];
-
-   var output = "<div class='char-img-div'>";
-
+function getCharImgString(charStr){
    switch (charStr){
-      case "Bowser": output += "<div class='char-Bowser'></div>"; break;
-      case "Captain Falcon": output += "<div class='char-Falcon'></div>"; break;
-      case "Donkey Kong": output += "<div class='char-DK'></div>"; break;
-      case "Dr. Mario": output += "<div class='char-Doc'></div>"; break;
-      case "Falco": output += "<div class='char-Falco'></div>"; break;
-      case "Fox": output += "<div class='char-Fox'></div>"; break;
-      case "Ganondorf": output += "<div class='char-Ganondorf'></div>"; break;
-      case "Ice Climbers": output += "<div class='char-ICs'></div>"; break;
-      case "Jigglypuff": output += "<div class='char-Jiggs'></div>"; break;
-      case "Kirby": output += "<div class='char-Kirby'></div>"; break;
-      case "Link": output += "<div class='char-Link'></div>"; break;
-      case "Luigi": output += "<div class='char-Luigi'></div>"; break;
-      case "Mario": output += "<div class='char-Mario'></div>"; break;
-      case "Marth": output += "<div class='char-Marth'></div>"; break;
-      case "Mewtwo": output += "<div class='char-Mewtwo'></div>"; break;
-      case "Mr. Game & Watch": output += "<div class='char-GW'></div>"; break;
-      case "Ness": output += "<div class='char-Ness'></div>"; break;
-      case "Peach": output += "<div class='char-Peach'></div>"; break;
-      case "Pikachu": output += "<div class='char-Pika'></div>"; break;
-      case "Pichu": output += "<div class='char-Pichu'></div>"; break;
-      case "Random": output += "<div class='char-Random'></div>"; break;
-      case "Roy": output += "<div class='char-Roy'></div>"; break;
-      case "Samus": output += "<div class='char-Samus'></div>"; break;
-      case "Sheik": output += "<div class='char-Sheik'></div>"; break;               
-      case "Yoshi": output += "<div class='char-Yoshi'></div>"; break;
-      case "Young Link": output += "<div class='char-YLink'></div>"; break;
-      case "Zelda": output += "<div class='char-Zelda'></div>"; break;
+      case "Bowser": return "<div class='char-Bowser'></div>";
+      case "Captain Falcon": return "<div class='char-Falcon'></div>";
+      case "Donkey Kong": return "<div class='char-DK'></div>";
+      case "Dr. Mario": return "<div class='char-Doc'></div>";
+      case "Falco": return "<div class='char-Falco'></div>";
+      case "Fox": return "<div class='char-Fox'></div>";
+      case "Ganondorf": return "<div class='char-Ganondorf'></div>";
+      case "Ice Climbers": return "<div class='char-ICs'></div>";
+      case "Jigglypuff": return "<div class='char-Jiggs'></div>";
+      case "Kirby": return "<div class='char-Kirby'></div>";
+      case "Link": return "<div class='char-Link'></div>";
+      case "Luigi": return "<div class='char-Luigi'></div>";
+      case "Mario": return "<div class='char-Mario'></div>";
+      case "Marth": return "<div class='char-Marth'></div>";
+      case "Mewtwo": return "<div class='char-Mewtwo'></div>";
+      case "Mr. Game & Watch": return "<div class='char-GW'></div>";
+      case "Ness": return "<div class='char-Ness'></div>";
+      case "Peach": return "<div class='char-Peach'></div>";
+      case "Pikachu": return "<div class='char-Pika'></div>";
+      case "Pichu": return "<div class='char-Pichu'></div>";
+      case "Random": return "<div class='char-Random'></div>";
+      case "Roy": return "<div class='char-Roy'></div>";
+      case "Samus": return "<div class='char-Samus'></div>";
+      case "Sheik": return "<div class='char-Sheik'></div>";
+      case "Yoshi": return "<div class='char-Yoshi'></div>";
+      case "Young Link": return "<div class='char-YLink'></div>";
+      case "Zelda": return "<div class='char-Zelda'></div>";
       default: break;
    }
+}
 
-   if (dataArray[1] == 0){
-      output += "<div class='tint-div'></div>";
-   }
+function getCharImgStringFromArray(dataArray){
+   var charStr = dataArray[0];
+   var output = "<div class='char-img-div'>";
+   var output = output + getCharImgString(charStr);
+   if (dataArray[1] == 0) output += "<div class='char-tint-div'></div>";
    output += "</div>";
    return output;
 
@@ -197,13 +197,13 @@ function renderHTML(data){
 
          htmlString += "<div class='p1chars-div'>";
          for (j=0; j<data[i].p1_characters.length; j++){
-            htmlString += getCharImgString(data[i].p1_characters[j]);
+            htmlString += getCharImgStringFromArray(data[i].p1_characters[j]);
          }
          htmlString += "</div>";
 
          htmlString += "<div class='p2chars-div'>";
          for (j=0; j<data[i].p2_characters.length; j++){
-            htmlString += getCharImgString(data[i].p2_characters[j]);
+            htmlString += getCharImgStringFromArray(data[i].p2_characters[j]);
          }
          htmlString += "</div>";
 
@@ -241,9 +241,11 @@ function insertIntoStatsObject(yourCharacter, opponentCharacter, opponentMedal, 
    if (youWon){
       statsObject[matchup].wins["total"]++;
       statsObject[matchup].stages[getStageShorthand(stage)].wins[opponentMedal][opponentDivision]++;
+      statsObject[matchup].stages[getStageShorthand(stage)].wins['total']++;
    }else{
       statsObject[matchup].losses["total"]++;
       statsObject[matchup].stages[getStageShorthand(stage)].losses[opponentMedal][opponentDivision]++;
+      statsObject[matchup].stages[getStageShorthand(stage)].losses['total']++;
    }
 
 }
@@ -291,12 +293,204 @@ function getStatsFromJSON(data){
    }
 }
 
+
+function isInt(n){
+   return n % 1 == 0;
+}
+
+// rounds floats to 3 decimal places but not full numbers
+function roundFloatsOnly(x){
+   if (isInt(x)) return x;
+   else return x.toFixed(3);
+}
+
+function capitalizeString(str){
+   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function getSmallMedalImgString(medalStr){
+   var output = "";
+
+   switch (medalStr){
+      case "bronze": output += "<div class='medal-bronze-small'></div>"; break;
+      case "silver": output += "<div class='medal-silver-small'></div>"; break;
+      case "gold": output += "<div class='medal-gold-small'></div>"; break;
+      case "platinum": output += "<div class='medal-platinum-small'></div>"; break;
+      case "master": output += "<div class='medal-master-small'></div>"; break;
+      case "grandsmasher": output += "<div class='medal-grandsmasher-small'></div>"; break;
+      case "unranked": output += "<div class='medal-unranked-small'></div>"; break;
+   }
+
+   return output;
+}
+
+
+function getTableStringFromStatsChart(chart){
+   var outHTML = "";
+
+   outHTML += "<table class='stats-table'>";
+      outHTML += "<thead>";
+         outHTML += "<tr>";
+            outHTML += "<th></th>";
+            outHTML += "<th>Wins</th>";
+            outHTML += "<th>Losses</th>";
+            outHTML += "<th>Total Games</th>";
+            outHTML += "<th>Winrate</th>";
+         outHTML += "</tr>";
+      outHTML += "</thead>";
+
+      outHTML += "<tbody>";
+         outHTML += "<tr class='highlight'>";
+            outHTML += "<td>" + "Total" + "</td>";
+            outHTML += "<td>" + chart.wins['total'] + "</td>";
+            outHTML += "<td>" + chart.losses['total'] + "</td>";
+            outHTML += "<td>" + chart.total_games.total + "</td>";
+            outHTML += "<td>" + roundFloatsOnly(chart.winrates.total) + " % </td>";
+         outHTML += "</tr>";
+
+         for (i=0; i<medals.length; i++){
+            outHTML += "<tr class='highlight'>";
+            outHTML += "<td>" + "vs. " + capitalizeString(medals[i])
+               + " " + getSmallMedalImgString(medals[i]) + "</td>";
+            outHTML += "<td>" + chart.wins[medals[i]] + "</td>";
+            outHTML += "<td>" + chart.losses[medals[i]] + "</td>";
+            outHTML += "<td>" + chart.total_games[medals[i]] + "</td>";
+            outHTML += "<td>" + roundFloatsOnly(chart.winrates[medals[i]]) + " % </td>";
+            outHTML += "</tr>";
+         }
+
+      outHTML += "</tbody>";
+
+   outHTML += "</table>";
+
+   return outHTML;
+}
+
+
+function getLeftCharacterFromMatchup(matchupStr){
+   var tokens = matchupStr.split(" ");
+   return tokens[0];
+}
+
+function getRightCharacterFromMatchup(matchupStr){
+   var tokens = matchupStr.split(" ");
+   var ret = "";
+   for (i=2; i<tokens.length - 1; i++){
+      ret += tokens[i] + " ";
+   }
+   ret += tokens[tokens.length-1];
+   return ret;
+}
+
+
+var stageTintDivCounter = 0;
+var stageTintDivStates = [];
+
+function addStageTintDivToggles(){
+   for (i=0; i<stageTintDivCounter; i++){
+      stageTintDivStates.push(0);
+
+      var btnid = "#stage-tint-div"+String(i);
+
+      var btn = $(btnid)[0];
+
+      btnid = "#stats-stage-btn"+String(i);
+      btn = $(btnid)[0];
+      btn.addEventListener("click", function(){;
+         var num = String(this.id).substring(15);
+         
+         if (stageTintDivStates[num] == 1){
+            //console.log("case 1", this);
+            var tmp = "#stage-tint-div"+String(num);
+            $(tmp).addClass('hidden');
+            stageTintDivStates[num] = 0;
+         }else{
+            //console.log("case 2", this);
+            var tmp = "#stage-tint-div"+String(num);
+            $(tmp).removeClass('hidden');
+            stageTintDivStates[num] = 1;
+         }
+
+      });
+   }
+
+
+
+}
+
+
+
 function renderStats(statsObject){
+   var outHTML = "";
+
    for (var matchup in statsObject){
       if (statsObject.hasOwnProperty(matchup)){
+
+         outHTML += "<div class='matchup-stats-div'>";
          console.log("matchup object:", statsObject[matchup]);
+
+         outHTML += "<div class='matchup-stats-div-header'>"
+            + getCharImgString(getLeftCharacterFromMatchup(matchup))
+            + " " + matchup + " "
+            + getCharImgString(getRightCharacterFromMatchup(matchup))
+            + "</div>";
+
+         outHTML += "<div class='matchup-stats-stages-div'>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-BF'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-FD'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-FoD'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-YS'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-PS'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "<div class='stats-stage-btn' id='stats-stage-btn" + stageTintDivCounter + "'>" + "<div class='stage-DL'></div>"
+            + "<div id='stage-tint-div" + stageTintDivCounter++ + "' "
+            + "class='stage-tint-div hidden'></div>" + "</div>";
+         outHTML += "</div>";
+
+         var sc_FD = new statsChart();
+         var sc_BF = new statsChart();
+         var sc_YS = new statsChart();
+         var sc_PS = new statsChart();
+         var sc_FoD = new statsChart();
+         var sc_DL = new statsChart();
+
+         populateStatsChart(statsObject[matchup].stages['FD'], sc_FD);
+         populateStatsChart(statsObject[matchup].stages['BF'], sc_BF);
+         populateStatsChart(statsObject[matchup].stages['YS'], sc_YS);
+         populateStatsChart(statsObject[matchup].stages['PS'], sc_PS);
+         populateStatsChart(statsObject[matchup].stages['FoD'], sc_FoD);
+         populateStatsChart(statsObject[matchup].stages['DL'], sc_DL);
+
+         console.log("BF chart: ", sc_BF);
+         console.log("FD chart: ", sc_FD);
+         console.log("DL chart: ", sc_DL);
+         console.log("YS chart: ", sc_YS);
+         console.log("FoD chart: ", sc_FoD);
+         console.log("PS chart: ", sc_PS);
+
+         var chart = new statsChart();
+         chart = addStatsChart(sc_FD, sc_BF);
+         chart = addStatsChart(chart, sc_YS);
+         chart = addStatsChart(chart, sc_PS);
+         chart = addStatsChart(chart, sc_FoD);
+         chart = addStatsChart(chart, sc_DL);
+
+         outHTML += getTableStringFromStatsChart(chart);
+         outHTML += "</div>";
       }
    }
+
+   statsDiv.insertAdjacentHTML('beforeend', outHTML);
+   addStageTintDivToggles();
 }
 
 function main(){
---
+++ dist/js/stats-chart.js
@@ -0,0 +1,90 @@
+// constructor
+function statsColumn(){
+   this.total = 0;
+   this.unranked = 0;
+   this.bronze = 0;
+   this.silver = 0;
+   this.gold = 0;
+   this.platinum = 0;
+   this.master = 0;
+   this.grandsmasher = 0;
+}
+
+// constructor
+function statsChart(){
+   this.wins = new statsColumn();
+   this.losses = new statsColumn();
+   this.total_games = new statsColumn();
+   this.winrates = new statsColumn();
+}
+
+var medals = ['unranked', 'bronze', 'silver', 'gold', 'platinum', 'master', 'grandsmasher'];
+
+// Populates statsChart based on the data in stageObject
+// Example of a stageObject: statsObject[matchup].stages[stage]
+function populateStatsChart(stageObject, statsChart){
+   // console.log("populateStatsChart: stageObject:", stageObject);
+   // console.log("populateStatsChart: statsChart:", statsChart);
+
+   for (i=0; i<medals.length; i++){
+
+      if (medals[i] == 'unranked'){
+         statsChart.wins.unranked += stageObject.wins.unranked['---'];
+         statsChart.losses.unranked += stageObject.losses.unranked['---'];
+      }else{
+         statsChart.wins[medals[i]] += stageObject.wins[medals[i]]['I'];
+         statsChart.wins[medals[i]] += stageObject.wins[medals[i]]['II'];
+         statsChart.wins[medals[i]] += stageObject.wins[medals[i]]['III'];
+
+         statsChart.losses[medals[i]] += stageObject.losses[medals[i]]['I'];
+         statsChart.losses[medals[i]] += stageObject.losses[medals[i]]['II'];
+         statsChart.losses[medals[i]] += stageObject.losses[medals[i]]['III'];          
+      }
+
+      statsChart.wins.total += statsChart.wins[medals[i]];
+      statsChart.losses.total += statsChart.losses[medals[i]];
+
+      statsChart.total_games[medals[i]] = statsChart.wins[medals[i]] + statsChart.losses[medals[i]];
+      statsChart.total_games.total += statsChart.total_games[medals[i]];
+
+      if (statsChart.total_games[medals[i]] == 0){
+         statsChart.winrates[medals[i]] = 0;
+      }else{
+         statsChart.winrates[medals[i]] = (statsChart.wins[medals[i]]/statsChart.total_games[medals[i]])*100;         
+      }
+   }
+}
+
+
+
+// Subtracts scB's values from scA and returns the result
+// i.e. returns scA - scB
+function subtractStatsChart(scA, scB){
+   return;
+}
+
+// Adds scA's and scB's values and returns the result
+// i.e. returns scA + scB
+function addStatsChart(scA, scB){
+   var result = new statsChart();
+
+   result.wins['total'] = scA.wins['total'] + scB.wins['total']; // need parseInt?
+   result.losses['total'] = scA.losses['total'] + scB.losses['total'];
+   result.total_games.total += result.wins['total'] + result.losses['total'];
+   if (result.total_games.total == 0) result.winrates.total = 0;
+   else result.winrates.total = (result.wins['total']/result.total_games.total)*100;
+
+   for (i=0; i<medals.length; i++){
+      result.wins[medals[i]] = scA.wins[medals[i]] + scB.wins[medals[i]];
+      result.losses[medals[i]] = scA.losses[medals[i]] + scB.losses[medals[i]];
+      result.total_games[medals[i]] = result.losses[medals[i]] + result.wins[medals[i]];
+      if (result.total_games[medals[i]] == 0){
+         result.winrates[medals[i]] = 0;
+      }else{
+         result.winrates[medals[i]] = (result.wins[medals[i]]/result.total_games[medals[i]])*100;
+      }
+      
+   }
+
+   return result;
+}
