b390dd0a53bf053bf97a412c4bb3c68074edfd58
scMarth <vlantaca@gmail.com>
2017-06-07 21:11:31 -0700

    cleanup

--- source/htmlMatchParser.bat
+++
@@ -1,7 +0,0 @@
-del "C:\MinGW\bin\mySmashStats\source\htmlMatchParser.exe"
-PAUSE
-copy "C:\MinGW\bin\mySmashStats\visual-studio\htmlMatchParser\Release\htmlMatchParser.exe" "C:\MinGW\bin\mySmashStats\source\htmlMatchParser.exe"
-rem PAUSE
-call htmlMatchParser > catchoutput
-
-PAUSE
